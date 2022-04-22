51849d97d182f8176eac94092ee7a136cd0f89d6
Cured-2000 <nickespinosa83@gmail.com>
2022-04-22 13:32:11 -0500

    added rough draft of algorithm

--- main.py
+++ main.py
@@ -5,24 +5,44 @@
 
 def deadlock_detector(instructions):
     #define variables
+    # fill process matrix
+    process_matrix = []
+    for i in instructions[3:]:
+        row = [int(m) for m in i if m.isdigit()]
+        process_matrix.append(row)
+
     num_processes = [int(m)for m in instructions[0] if m.isdigit()][0]
 
     num_resources = [int(m)for m in instructions[1] if m.isdigit()][0]
 
     resource_cap = [int(m)for m in instructions[2] if m.isdigit()]
 
-    process_matrix =[]
+    #initalize work and finish matrix
+    work_matrix = [0 for col in range(num_resources)]
+    finish_matrix = [False for col in range(num_processes)]
 
-    #fill matrix
-    for i in instructions[3:]:
-        row = [int(m)for m in i if m.isdigit()]
-        process_matrix.append(row)
-    matrix_length = len(process_matrix)-1
+    #find currently allocated blocks by process
+    allocation_matrix = []
+    for i in range(len(process_matrix[num_processes:])):
+        allocation_matrix.append([row[i]for row in process_matrix[num_resources:]])
 
+    #find requested resources by process
+    request_matrix = []
+    for i in process_matrix[:num_processes]:
+        request_matrix.append(i[num_resources:])
 
+    #find current availability
+    total_alloc = [sum(x) for x in zip(*allocation_matrix)]
+    avail_matrix = [x-y for x, y in zip(resource_cap, total_alloc)]
 
+    #iterate through matrix and find knots(Needs Work!)
+    for i in range(len(request_matrix)):
+        if request_matrix[i] <= work_matrix and finish_matrix[i] is False:
 
-    print(process_matrix)
+            work_matrix = [x+y for x, y in zip(allocation_matrix[i], work_matrix)]
+            finish_matrix[i] = True
+
+    print(finish_matrix)
 
 def read_file():
     #define list of instructions
